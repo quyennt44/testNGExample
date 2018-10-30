593614dbd966c6e68e30399f8dfb59459e3aef1d
QuyênNT <quyennt44@gmail.com>
2018-10-30 14:07:55 +0700

    add some methods

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenExample.java
@@ -8,4 +8,35 @@ public class TestNgMavenExample {
 	public void exampleOfTestNgMaven() {
 		System.out.println("This is TestNG-Maven Example");
 	}
+	
+	@Test
+	public void test1() {
+		System.out.println("This is test 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("This is test 2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("This is test 3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("This is test 4");
+	}
+	
+	@Test
+	public void test5() {
+		System.out.println("This is test ");
+	}
+	
+	@Test
+	public void test6() {
+		System.out.println("This is test 6");
+		assert(false);
+	}
 }
